c28122c20d40630bcab0c5480e7e41f6860e97bc
Kate1863 <masik01091981@gmail.com>
2023-09-24 23:14:38 +0200

    settings.py

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = local_settings.DEBUG
 
-ALLOWED_HOSTS = ["127.0.0.1", "KateBel.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
