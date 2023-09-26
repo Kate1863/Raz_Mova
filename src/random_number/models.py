7970d5b8de8a5dc990198092ab43554cb642df23
Kate1863 <masik01091981@gmail.com>
2023-09-26 21:49:13 +0200

    commit writers

--- src/random_number/models.py
+++ src/random_number/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Writer(models.Model):
+    first_name = models.CharField(
+        verbose_name="First Name", 
+        max_length=50
+        )
+    last_name = models.CharField(
+        verbose_name="Last Name", 
+        max_length=50
+        )
+    
+    genre = models.CharField(
+        verbose_name="Genre",
+        max_length=50,
+        null=True,
+        blank=True
+    )    
+
+    def __str__(self):
+        return f"Writer {self.first_name} {self.last_name}"
+    
